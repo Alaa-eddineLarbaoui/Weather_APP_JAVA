41e1496c9639151c9ae93907085e5ebb64c8ed0c
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-02-20 10:58:34 +0100

    j'ai crée deux classes : classe City et City History

---
+++ src/City.java
@@ -0,0 +1,30 @@
+
+
+
+public class City {
+
+    String cityId;     //Identifiant unique pour la ville.
+    String cityName;     //Nom de la ville.
+    double currentTemperature;    //Température actuelle.
+    double currentHumidity;   //Taux d'humidité actuelle.
+    String currentWindSpeed;  //Vitesse du vent actuelle.
+
+
+    public void Ajouter() {
+
+    }
+
+    public void Afficher(){
+
+    }
+
+    public void Modifier(){
+
+    }
+
+    public void Supprimer(){
+
+    }
+
+
+}
---
+++ src/City_History.java
@@ -0,0 +1,25 @@
+public class City_History {
+
+
+    private int historicalDataId;  //Identifiant unique pour les données historiques.
+    private int cityId;          //Identifiant de la ville associée.
+    private String eventDate;    //Date de l'événement météorologique historique.
+    private int temperature;    //Température historique
+
+
+    public void AjouterHisto(){
+
+    }
+    public void AfficherHisto() {
+
+    }
+
+    public void SupprimerHisto() {
+
+    }
+
+    public void ModifierHisto() {
+    }
+
+
+}
